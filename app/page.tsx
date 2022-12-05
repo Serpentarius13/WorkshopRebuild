e692415efe9d7cebfdf5f5585e3b5da36e87e4a2
Serpentarius13 <serpentarium13@mail.ru>
2022-12-05 03:23:33 +0300

    animations

--- app/page.tsx
+++ app/page.tsx
@@ -12,19 +12,19 @@ export default async function Home() {
   return (
     <>
       <HomeHeader />
-      <div className="container mx-auto p-8 flex flex-col items-center space-y-8">
+      <div className="container mx-auto p-8 flex flex-col items-center justify-center space-y-8">
         {" "}
         <p className="italic text-center w-[60%]  mx-auto   ">
           {" "}
           Since ancient times people believed that dreams have some extra sense
           to them. Wise philosophers and artisans of the past admitted that
-          dreams brought them revelations and knowledge. From 20th century it
-          is thought that dreams is something beyond normal waking life - a
-          world of ghosts that affects our personality and motivations. There is
+          dreams brought them revelations and knowledge. From 20th century it is
+          thought that dreams is something beyond normal waking life - a world
+          of ghosts that affects our personality and motivations. There is
           plenty of evidence that dreams are not just mere illusions - they have
           sense.{" "}
         </p>{" "}
-        <h3 className="text-6xl text-center tracking-tight underline">
+        <h3 className="text-6xl text-center tracking-tight underline text-purple-800">
           {" "}
           look for yourself{" "}
         </h3>
--- components/homePage/homeTestimonial.tsx
+++ components/homePage/homeTestimonial.tsx
@@ -1,4 +1,6 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useEffect, useRef } from "react";
 import Image from "next/image";
 
 interface HomeTestimonial {
@@ -14,9 +16,31 @@ const HomeTestimonial: FC<HomeTestimonial> = ({
   reversed = false,
   imageSource,
 }) => {
+  const ref = useRef<any>(null);
+
+  const animate = () => {
+    if (!ref) return;
+    const offset = window.pageYOffset;
+    const y = ref.current.getBoundingClientRect().y;
+
+    if (offset + 300 > y) {
+      ref.current.classList.remove("hidden");
+      ref.current.classList.add("appear");
+      document.removeEventListener("scroll", animate);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("scroll", animate);
+
+    return () => document.removeEventListener("scroll", animate);
+  }, []);
   return (
     <div
-      className={` max-w-[70rem]  flex  ${reversed ? "flex-row-reverse" : ""}`}
+      ref={ref}
+      className={`notShown max-w-[70rem]  flex  ${
+        reversed ? "flex-row-reverse" : ""
+      }`}
     >
       <div className="w-[80px] flex items-center justify-center  ">
         <div className="w-24 h-18 overflow-hidden  rounded-full flex items-center justify-center ">
