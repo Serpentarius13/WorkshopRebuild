120de685920c275c7b6ade1538573d5870e06eec
Serpentarius13 <serpentarium13@mail.ru>
2022-12-10 03:18:33 +0300

    testi fix

--- components/homePage/homeTestimonial.tsx
+++ components/homePage/homeTestimonial.tsx
@@ -23,22 +23,22 @@ const HomeTestimonial: FC<HomeTestimonial> = ({
     <HomeBlurAppear>
       <div
         ref={ref}
-        className={` max-w-[70rem]  flex  ${
+        className={` max-w-[70rem]  flex bg-gray-900  px-8 py-2 rounded-xl  ${
           reversed ? "flex-row-reverse" : ""
         }`}
       >
         <div className="w-[80px] flex items-center justify-center  ">
-          <div className="w-24 h-18 overflow-hidden  rounded-full flex items-center justify-center ">
+          <div className="w-24 h-18 overflow-hidden  rounded-full flex items-center justify-center border-orange-400 border-4   ">
             <Image
               src={imageSource}
               width={96}
               height={96}
               alt={imageSource.split(".")[0].replace("/", "")}
-              className=" min-w-[100%] min-h-[85%]"
+              className=" min-w-[100%] min-h-[85%] "
             />{" "}
           </div>
         </div>
-        <div className="flex-1  p-6 my-auto space-y-2">
+        <div className="flex-1  p-6 my-auto space-y-2 text-white text-2xl z-[500]">
           <p className="italic">
             {text}
 
