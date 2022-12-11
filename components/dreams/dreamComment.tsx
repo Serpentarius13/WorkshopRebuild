46b9f56c97c6eeae00a854eef244b164e1a9e03a
Serpentarius13 <serpentarium13@mail.ru>
2022-12-11 15:54:57 +0300

    !

--- components/dreams/dreamComment.tsx
+++ components/dreams/dreamComment.tsx
@@ -60,7 +60,9 @@ const Comment = ({ comment, level = 0, likeHandler, refetch }) => {
           rating={commentRating}
           condition={likedBy.includes(id)}
           size={14}
-          handler={async () => await likeHandler(false, _id)}
+          handler={async () =>
+            await likeHandler(false, _id).then((_) => refetch())
+          }
         />{" "}
       </div>
       <p className="break-words "> {commentText} </p>{" "}
