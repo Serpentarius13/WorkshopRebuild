ffb0e93fb59c5181bb24c4aed9d85511c16be995
Serpentarius13 <serpentarium13@mail.ru>
2022-12-12 17:04:57 +0300

    !

--- app/_app.tsx
+++ app/_app.tsx
@@ -1,16 +1,12 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import { ApolloProvider } from "@apollo/client";
 
-import Head from "next/head";
-
-import { client } from "../apollo-client";
-import { userStore } from "../store/store";
-import { useEffect } from "react";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const { login } = userStore;
-
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {" "}
+      <Component {...pageProps} />{" "}
+    </>
+  );
 }
