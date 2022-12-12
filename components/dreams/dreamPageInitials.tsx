ff7fb2fde838037e9c42892401e51e88b03bdf0d
Serpentarius13 <serpentarium13@mail.ru>
2022-12-12 15:17:39 +0300

    !

--- components/dreams/dreamPageInitials.tsx
+++ components/dreams/dreamPageInitials.tsx
@@ -25,13 +25,15 @@ const DreamPageInitials = ({ id, name, time, avatar, authorId }) => {
     openModal(ModalTypes.SEND_EMAIL_TO_USER);
   };
 
+  const ElementLink = authorId ? ({children}) => <Link href={`/profile/${authorId}`}> {children} </Link> : ({children}) => <div> {children} </div>
+
   return (
     <div className="flex md:mr-24 space-x-4 items-center justify-center">
       {" "}
       {time || name ? (
         <div className="flex items-center justify-center space-x-2 mx-auto">
           {name ? (
-            <Link href={`/profile/${authorId}`}>
+             <ElementLink>
               {" "}
               <div className="px-6 py-3 text-blue-800 border-blue-800 border-4 items-center justify-center pr-4 flex space-x-2">
                 {" "}
@@ -62,7 +64,7 @@ const DreamPageInitials = ({ id, name, time, avatar, authorId }) => {
                   />
                 )}
               </div>{" "}
-            </Link>
+            </ElementLink>
           ) : (
             ""
           )}
